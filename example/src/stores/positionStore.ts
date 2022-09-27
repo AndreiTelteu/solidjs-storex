fb5f007d2760bdd61777432febe5f0159de4ed52
Andrei Telteu <andrei.telteu@touch-media.ro>
2022-09-28 02:46:04 +0300

    Example on how you would specify a custom type def

--- example/src/stores/positionStore.ts
+++ example/src/stores/positionStore.ts
@@ -1,14 +1,22 @@
 import { defineStore } from '../../../src/index';
 
+// this is how you would define custom type for every state if you need
+// this is optional
+type PositionState = {
+  pos: [number, number] | null;
+};
+
 export default defineStore({
   state: {
     pos: [0, 0],
-  },
+  } as PositionState,
+
   options: {
     persistent: true,
     storageKey: 'solid-demo-pos',
     storageThrottle: 500, // this will only write in localStorage every 500 ms
   },
+
   actions: (state, set) => ({
     move: (pos) => {
       set('pos', pos);
